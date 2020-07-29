dd17814cbbb00f7b689181fced3db3c4cd684717
SpAnDaNpAtIl <51505605+SpAnDaNpAtIl@users.noreply.github.com>
2020-07-29 20:45:02 +0530

    data of your headers and new email ID.

---
+++ data_for_instiapp.py
@@ -0,0 +1,22 @@
+
+header = {"authority": "www.insti.app",
+"method": "GET",
+"path": "/api/training-blog",
+"scheme": "https",
+"accept": "application/json, text/plain, */*",
+"accept-encoding": "gzip, deflate, br",
+"accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+"cookie": "sessionid=g3k3svjs33urol28b9f5zblnf5v1xj2d",
+"referer": "https://www.insti.app/blog/ImFwaS90cmFpbmluZy1ibG9nez9mcm9tLG51bSxxdWVyeX0i",
+"sec-fetch-dest": "empty",
+"sec-fetch-mode": "cors",
+"sec-fetch-site": "same-origin",
+"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+
+
+email_ID_used_for_sending_mails = 'spandyinstiapp@gmail.com'
+password = 'InstiApp'
+
+email_ID_to_send = 'spandanp155@gmail.com'
+
+
